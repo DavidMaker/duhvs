7cf5900cb48b4f0b406d8e13a1263bff23afc90f
David Givens <davidgivens@Davids-MacBook-Air.local>
2018-12-02 06:00:34 -0600

    Generate Pages

---
+++ app/helpers/neworks_helper.rb
@@ -0,0 +1,2 @@
+module NeworksHelper
+end
---
+++ test/controllers/neworks_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class NeworksControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
